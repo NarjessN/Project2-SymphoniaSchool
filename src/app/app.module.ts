c879cb5e954ab1ac798cc5fb3c70e568b9d8c195
NarjessNa <narje1234@gmail.com>
2020-08-07 01:01:24 +0300

    create component form introduction , sing-in , sing-up fic in html code for sing-in and introduction link using router nbetween sing-in and ntroduction we still have probleme with link router with sing-up we still try to fix it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { SingInComponent } from './sing-in/sing-in.component';
+import { SingUpComponent } from './sing-up/sing-up.component';
+const app_router :Routes=[
+{path:'' ,component: AppComponent},
+{path: 'sing-in', component :SingInComponent},
+{path:'sing-up',component:SingUpComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+     SingUpComponent,
+     SingInComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(app_router)
   ],
   providers: [],
   bootstrap: [AppComponent]
