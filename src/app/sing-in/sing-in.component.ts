4e058cd3c04e0d80905783519b7f5895b35977b7
NarjessNa <narje1234@gmail.com>
2020-09-08 14:41:22 +0300

    solve a probleme in sing-up , store data we get from the sing-up form  and store the data we get from the sing-in form , get the data ready for sent in the sing-in and sing-up and try to send with http request firebase envirment

--- src/app/sing-in/sing-in.component.ts
+++ src/app/sing-in/sing-in.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import {NgForm, NgModel } from '@angular/forms'
 import { __values } from 'tslib';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-sing-in',
@@ -12,23 +13,53 @@ import { HttpClient } from '@angular/common/http';
 export class SingInComponent  {
   private email : String ;
   private password : String ;
+public  map: Map<String , any> = new Map<String,any>();
 
- private Map <String , dynamic> (authdata): any {
-'email';
-'password';
- } ;
-  constructor(private http : HttpClient) { }
+  constructor(private http : HttpClient) {
+   
+  }
 
   ngOnInit(): void {
   }
  
   onSubmit(form :NgForm){
-    console.log(form);
-  temp : JSON;
-    const temp=form.value;
+    this.email=form.controls.email.value;
+    this.password=form.controls.password.value;
+    let temp ={"email": this.email,"password":this.password}
+    for (let member in temp) {
+      this.map.set(member, temp[member]);
+    }
+  console.log(this.map);
+    // mymap.set("email",form.controls.email.value);
+     //mymap.set("password",form.controls.password.value);
+     /*for (let key of mymap.keys()) {  
+      console.log("Map Keys= " +key);          
+  } */
+  /*  for (let value of  mymap.values()) {  
+    console.log("Map Values= " +value);      
+}  */
+
+   
+   // console.log(form.controls.email.value);
+    //console.log(form.controls.password.value);
+
+    //const temp=form.value;
+//this.Map["email"]=form.controls.email.value;
+//this.Map["password"]=form.controls.password.value;
+//console.log(this.Map["email"]);
    // console.log( temp);
  //this.http.post('http://authentication-2c48e.firebaseapp.com',temp).subscribe(
    //responseData =>{console.log(responseData)});
+   
+   //const  myObjStr  = JSON.stringify(temp);
+   //console.log(myObjStr);
+   let p = new HttpHeaders({headers:"Access-Control-Allow-Origin': 'https://symphonia-school.firebaseapp.com/__/auth/handler"})
+   this.http.post('https://symphonia-school.firebaseapp.com/__/auth/handler',
+   JSON.stringify(temp),{headers : p }).subscribe(responseData =>{
+     console.log(responseData);
+   } )  ;
+   
+;
 
     
 }
--- src/app/sing-up/sing-up.component.ts
+++ src/app/sing-up/sing-up.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
+import { HttpClientModule, HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
+import { NgbCheckBox } from '@ng-bootstrap/ng-bootstrap';
+import { isInteger } from '@ng-bootstrap/ng-bootstrap/util/util';
+import { Options } from 'selenium-webdriver/chrome';
+
 
 @Component({
   selector: 'app-sing-up',
@@ -8,16 +12,131 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
   styleUrls: ['./sing-up.component.css']
 })
 export class SingUpComponent implements OnInit {
+@ViewChild('f') singupform : NgForm
+  private email : String ;
+  private password : String ;
+ private school_name : String ;
+ private first_telephone_number : String;
+ private seconde_telepjone_number : String ;
+ private school_address : String 
+ private male_school_type : boolean ;
+ private female_school_type : boolean;
+ private first_study_level : boolean ;
+ private seconde_study_level : boolean ;
+ //private third_study_level : boolean ;
+ private third_school_level_litt : boolean;
+ private third_school_level_SCi: boolean  ;
+//private third_school_level_SCi_litt : boolean ;
+public  check_the_value  ;
+ public last_value = false ;
+ public counter=0;
+  public temp 
+public    array_for_input_field  =new Array<number>() ;
 
-  constructor ( private http : HttpClient) { }
+
+  //public  map: Map<String , any> = new Map<String,any>();
+  constructor ( private http : HttpClient) {
+ 
+  }
 
   ngOnInit(): void {
   }
+
 onSubmite(form: NgForm){
-console.log(form)
+//console.log(this.singupform)
 // this.http.post('https://symphonia-school.firebaseio.com/posts.json'
 // ,form).subscribe(responseData =>{
 //   console.log(responseData);
 // } )
+
+if((form.controls.male.valid ||form.controls.femal_school_type.valid) 
+&& (form.controls.frist_study_level.valid ||form.controls.seconde_study_level.valid
+  || form.controls.third_school_level_litt.valid 
+  ||form.controls.third_school_level_SCi.valid
+    )
+    && ( form.controls.email.valid) &&(form.controls.password.valid)
+     &&(form.controls.confirmpassword.valid) &&(form.controls.password.value ===form.controls.confirmpassword.value)
+     &&(form.controls.schooltelephone.valid) 
+     &&(form.controls.schooltelephone2.valid)
+     &&(form.controls.schooladdress.valid)
+)
+{
+  //this.sending_data(form);
+  this.email=form.controls.email.value;
+this.password=form.controls.password.value;
+this.school_name=form.controls.schoolname.value;
+this.first_telephone_number= form.controls.schooltelephone.value;
+this.seconde_telepjone_number = form.controls.schooltelephone2.value;
+this.male_school_type=form.controls.male.value;
+this.female_school_type = form.controls.femal_school_type.value;
+this.school_address=form.controls.schooladdress.value;
+this.first_study_level=form.controls.frist_study_level.value;
+this.seconde_study_level = form.controls.seconde_study_level.value;
+//this.third_study_level = form.controls.third_school_level.value;
+this.third_school_level_litt= form.controls.third_school_level_litt.value;
+this.third_school_level_SCi = form.controls.third_school_level_SCi.value;
+//this.third_school_level_SCi_litt = form.controls.third_school_level_SCi_litt.value;
+//this.schoolame=form.controls
+this.temp ={"email": this.email,"password":this.password,"school_name":this.school_name, 
+"school_address": this.school_address,
+"first_school_telephone" : this.first_telephone_number,
+"seconde_telephone_number":this.seconde_telepjone_number,
+"male_school_type": this.male_school_type,
+"female_school_type" : this.female_school_type,
+"first_study_level": this.first_study_level,
+"seconde_study_level": this.seconde_study_level,
+//"third_study_level" : this.third_study_level,
+"third_school_level_litt": this.third_school_level_litt,
+"third_school_level_SCi" : this.third_school_level_SCi,
+//"third_school_level_SCi_litt":this.third_school_level_SCi_litt
+} 
+/* for (let member in temp) {
+      this.map.set(member, temp[member]);
+
+    } 
+  
+    console.log(this.map) */
+
+  //  console.log(JSON.stringify(temp))
+    this.check_the_value=true 
+ 
+   // this.sending_data();
+   console.log(this.temp)
+}
+else {
+ return " the data will not me sending there is still missing value we have " 
+}
+
+}
+/*erro_input(  ){
+
+this.check_the_value=false ;
+return this.check_the_value
+}
+correct_input()
+{
+ 
+  this.check_the_value=true;
+  console.log(this.check_the_value)
+ // this.array_for_input_field.push(this.check_the_value);
+//this.counter++;
+//console.log(this.counter);
+/*if(this.counter===this.array_for_input_field.length)
+{
+  this.last_value=true ;
+  return this.check_the_value
+}*/
+sending_data(){
+ // console.log(" we are in the sebding data function ")
+
+ let p = new HttpHeaders({headers:"Access-Control-Allow-Origin': 'https://symphonia-school.firebaseapp.com/__/auth/handler"})
+
+ 
+
+  this.http.post('https://symphonia-school.firebaseapp.com/__/auth/handler',
+  JSON.stringify(this.temp),{headers : p }).subscribe(responseData =>{
+    console.log(responseData);
+  } )  ;
+
 }
 }
